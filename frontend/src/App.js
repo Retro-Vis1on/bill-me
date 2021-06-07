e04af6a166e2ac1b8d9f5a5c1226e24e75219921
Pratham Singh <prathamsingh212@gmail.com>
2021-06-07 21:14:27 +0530

    Added lazy Loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,45 @@
-import './App.css';
 import { Redirect, Route, Switch } from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import InvoicePage from './pages/InvoicePage';
-import { useContext } from 'react';
+import React, { useContext, Suspense } from 'react';
 import AuthContext from './store/AuthContext';
-import SideBar from './components/UI/Sidebar/Sidebar';
-import UserPage from './pages/UserPage';
+import LoadingSpinner from './components/UI/LoadingSpinner/LoadingSpinner';
+import classes from './App.module.css';
 require('dotenv').config()
+const LoginPage = React.lazy(() => import('./pages/LoginPage'))
+const UserPage = React.lazy(() => import('./pages/UserPage'))
+const InvoicePage = React.lazy(() => import('./pages/InvoicePage'))
+const SideBar = React.lazy(() => import('./components/UI/Sidebar/Sidebar'))
+const HomePage = React.lazy(() => import('./pages/HomePage'))
 function App() {
   const ctx = useContext(AuthContext);
   return (
     <>
-      {ctx.token && <SideBar />}
-      <Switch>
-        <Route path="/" exact>
-          {ctx.token && ctx.isValid() ? <Redirect to="/home" /> : <LoginPage />}
-        </Route>
-        <Route path="/home" exact>
-          {!ctx.token || !ctx.isValid() ? <Redirect to="/" /> : <HomePage />}
-        </Route>
-        <Route path="/invoice/:id" >
-          {!ctx.token || !ctx.isValid() ? <Redirect to="/" /> : <InvoicePage />}
-        </Route>
-        <Route path="/invoice/:id" >
-          {!ctx.token || !ctx.isValid() ? <Redirect to="/" /> : <InvoicePage />}
-        </Route>
-        <Route path="/profile" exact>
-          {!ctx.token || !ctx.isValid() ? <Redirect to="/" /> : <UserPage />}
-        </Route>
-        <Route path="*">
-          {ctx.token && ctx.isValid() ? <Redirect to="/home" /> : <LoginPage />}
-        </Route>
-      </Switch>
+      <Suspense fallback={
+        <div className={classes.centered}>
+          <LoadingSpinner />
+        </div>
+      }>
+        {ctx.token && <SideBar />}
+        <Switch>
+          <Route path="/" exact>
+            {ctx.token && ctx.isValid() ? <Redirect to="/home" /> : <LoginPage />}
+          </Route>
+          <Route path="/home" exact>
+            {!ctx.token || !ctx.isValid() ? <Redirect to="/" /> : <HomePage />}
+          </Route>
+          <Route path="/invoice/:id" >
+            {!ctx.token || !ctx.isValid() ? <Redirect to="/" /> : <InvoicePage />}
+          </Route>
+          <Route path="/invoice/:id" >
+            {!ctx.token || !ctx.isValid() ? <Redirect to="/" /> : <InvoicePage />}
+          </Route>
+          <Route path="/profile" exact>
+            {!ctx.token || !ctx.isValid() ? <Redirect to="/" /> : <UserPage />}
+          </Route>
+          <Route path="*">
+            {ctx.token && ctx.isValid() ? <Redirect to="/home" /> : <LoginPage />}
+          </Route>
+        </Switch>
+      </Suspense>
     </>
   );
 }
